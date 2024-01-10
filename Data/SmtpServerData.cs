dd6a1d22f46c68e0c7b463c6d84544a75c1c7b16
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:49:45 +0100

    .

---
+++ Data/SmtpServerData.cs
@@ -0,0 +1,26 @@
+namespace SunamoMail.Data;
+
+/// <summary>
+/// Reason why is in desktop (derive from it SmtpData)
+/// </summary>
+public class SmtpServerData
+{
+    public string smtpServer { get; set; } = "smtp.gmail.com";
+    public int port { get; set; } = 587;
+
+    public static SmtpServerData Gmail()
+    {
+        var s = new SmtpServerData();
+        s.port = 587;
+        s.smtpServer = "smtp.gmail.com";
+        return s;
+    }
+
+    public static SmtpServerData SeznamCz()
+    {
+        var s = new SmtpServerData();
+        s.port = 25;
+        s.smtpServer = "smtp.seznam.cz";
+        return s;
+    }
+}
---
+++ GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using System.Collections.Generic;
+global using System.Linq;
+global using System.Net.Mail;
+global using System.Net;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System;
+global using SunamoMail.Data;
+global using SunamoStringSplit;
+global using SunamoValues;
