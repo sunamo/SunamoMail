95a9c55b73db92106760431deb312d66cab9f94b
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-28 23:05:54 +0200

    converted public to internal in _sunamo

--- _sunamo/SHSplit.cs
+++ _sunamo/SHSplit.cs
@@ -1,9 +1,9 @@
 using System.Linq;
 
 namespace SunamoMail;
-public class SHSplit
+internal class SHSplit
 {
-    public static List<string> Split(string p, params string[] newLine)
+    internal static List<string> Split(string p, params string[] newLine)
     {
         return p.Split(newLine, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
